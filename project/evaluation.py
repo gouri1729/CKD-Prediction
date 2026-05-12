871881ee73fe2bbbf0d37efd65e5ff146e2a19b5
Rima <newrima004@gmail.com>
2026-05-12 16:33:23 +0530

    Added project folder from gouri

---
+++ project/evaluation.py
@@ -0,0 +1,39 @@
+# evaluation.py
+
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
+from sklearn.model_selection import StratifiedKFold, cross_val_score
+import numpy as np
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    y_proba = model.predict_proba(X_test)[:, 1]
+    y_pred = (y_proba>0.3).astype(int)
+
+    report = classification_report(y_test, y_pred, output_dict=True)
+    cm = confusion_matrix(y_test, y_pred)
+    roc_auc = roc_auc_score(y_test, y_proba)
+
+    results = {
+        "Accuracy": report["accuracy"],
+        "Precision_CKD": report["1"]["precision"],
+        "Recall_CKD": report["1"]["recall"],
+        "F1_CKD": report["1"]["f1-score"],
+        "ROC_AUC": roc_auc
+    }
+    print("Unique predictions:", set(y_pred))
+    print("True labels:", set(y_test))
+    return results
+
+
+def cross_validate_model(model, X, y):
+    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+    scores = cross_val_score(
+        model,
+        X,
+        y,
+        cv=skf,
+        scoring="roc_auc"
+    )
+
+    return np.mean(scores), np.std(scores)
